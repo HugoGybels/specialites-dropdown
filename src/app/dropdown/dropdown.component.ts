72439b95f4da9f3b1f731c783e4448fc5578a7e6
HugoGybels <hugo4242@hotmail.fr>
2020-03-18 17:09:21 +0100

    3 files modified

--- src/app/dropdown/dropdown.component.ts
+++ src/app/dropdown/dropdown.component.ts
@@ -18,7 +18,12 @@ export class DropdownComponent implements OnInit {
       new Specialty(1, 'Libelle 1'),
       new Specialty(2, 'Libelle 2'),
       new Specialty(3, 'Libelle 3'),
-      new Specialty(4, 'Libelle 4')
+      new Specialty(4, 'Libelle 4'),
+      new Specialty(5, 'Libelle 5'),
+      new Specialty(6, 'Libelle 6'),
+      new Specialty(7, 'Libelle 7'),
+      new Specialty(8, 'Libelle 8'),
+      new Specialty(9, 'Libelle 9'),
     ];
 
     this.selectedSpecialties = this.specialties;
